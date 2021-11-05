2b6f7a3bb358ce164c595d91d7edc96c04034c87
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2021-11-06 02:37:28 +0600

    modified pokedex

--- Pokedex-Project/pokedex/src/components/Pokedex.js
+++ Pokedex-Project/pokedex/src/components/Pokedex.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Pokecard from './Pokecard'
+import './Pokedex.css'
 
 class Pokedex extends Component {
   static defaultProps = {
@@ -16,17 +17,19 @@ class Pokedex extends Component {
   }
 
   render() {
-    return(
-    <div className='Pokedex'>
-      <h1>Pokedex!</h1>
-      {this.props.pokemon.map((p) => (
-        <Pokecard
-          id={p.id}
-          name={p.name}
-          type={p.type}
-          exp={p.base_experience}
-        />
-      ))}
+    return (
+      <div className='Pokedex'>
+        <h1>Pokedex!</h1>
+        <div className='Pokedex-cards'>
+          {this.props.pokemon.map((p) => (
+            <Pokecard
+              id={p.id}
+              name={p.name}
+              type={p.type}
+              exp={p.base_experience}
+            />
+          ))}
+        </div>
       </div>
     )
   }
