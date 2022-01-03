a4609949aafd7d1809635874d8c8ab6d2506e263
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2022-01-04 00:11:59 +0600

    added link to react router dom

--- feedback-app/src/components/AboutIconLink.js
+++ feedback-app/src/components/AboutIconLink.js
@@ -1,9 +1,18 @@
 import { FaQuestion } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 function AboutIconLink() {
   return (
     <div className='about-link'>
-      <FaQuestion size={30} />
+      <Link
+        to={{
+          pathname: '/about',
+          search: '?sort=name',
+          hash: '#hello',
+        }}
+      >
+        <FaQuestion size={30} />
+      </Link>
     </div>
   )
 }
