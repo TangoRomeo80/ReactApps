d043c4e1a593c31bc7c0d13c592bd4baac21069c
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2022-01-03 13:36:41 +0600

    added validation

--- feedback-app/src/components/FeedbackItem.js
+++ feedback-app/src/components/FeedbackItem.js
@@ -3,7 +3,7 @@ import Card from './shared/Card'
 
 function FeedbackItem({ item, handleDelete }) {
   return (
-    <Card reverse={true}>
+    <Card reverse={false}>
       <div className='num-display'>{item.rating}</div>
       <button onClick={() => handleDelete(item.id)} className="close">
         <FaTimes color='purple' />
