49dfdc2bbddf328f0f4f0c6bfa0f0504eab02ca2
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2022-01-27 17:41:12 +0600

    updated navbar

---
+++ github-finder/src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaGithub } from 'react-icons/fa'
+import { link } from 'react-router-dom'
+
+function navbar({ title }) {
+  return <div>Navbar</div>
+}
+
+Navbar.defaultProps = {
+  title: 'Github Finder',
+}
+
+export default navbar
