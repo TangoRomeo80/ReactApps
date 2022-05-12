40c3a646872b881843206f0bb26eddced559f9a6
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2022-05-12 17:00:56 +0600

    added backed io structure

--- ChatApp/client/src/components/Chat/Chat.js
+++ ChatApp/client/src/components/Chat/Chat.js
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import io from 'socket.io-client'
+
+const ENDPOINT = 'localhost:5000'
+
+let socket
 
 const Chat = () => {
+  const [searchParams] = useSearchParams()
+
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+
+  useEffect(() => {
+    const { name, room } = Object.fromEntries([...searchParams])
+
+    setRoom(room)
+    setName(name)
+
+    socket = io(ENDPOINT)
+
+    socket.emit('join', { name, room }, (error) => {
+      if (error) {
+        alert(error)
+      }
+    })
+
+    return () => {
+      socket.disconnect()
+      socket.off()
+    }
+  }, [ENDPOINT, searchParams])
+
   return <h1>Chat</h1>
 }
 
--- ChatApp/server/index.js
+++ ChatApp/server/index.js
@@ -7,10 +7,23 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 const server = http.createServer(app)
-const io = socketio(server)
+const io = socketio(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    credentials: true,
+    allowEIO3: true,
+  },
+  transport: ['websocket'],
+})
 
 io.on('connection', (socket) => {
   console.log('We Have a new connection!!!')
+
+  socket.on('join', ({ name, room }, callback) => {
+    console.log(`User ${name} has joined Room ${room}`)
+  })
+
   socket.on('disconnect', () => {
     console.log('User has disconnected!!!')
   })
