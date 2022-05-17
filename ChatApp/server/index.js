8599d64f8713b8bd1840dab9fbcbffac0a31add6
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2022-05-17 17:27:44 +0600

    updated and finalized chat app

--- ChatApp/server/index.js
+++ ChatApp/server/index.js
@@ -18,9 +18,9 @@ const io = socketio(server, {
   transport: ['websocket'],
 })
 
-io.on('connection', (socket) => {
-  console.log('We Have a new connection!!!')
+app.use(router)
 
+io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room })
 
@@ -45,19 +45,28 @@ io.on('connection', (socket) => {
   })
 
   socket.on('sendMessage', (message, callback) => {
-    const user = getUser(socket.id);
+    const user = getUser(socket.id)
 
-    io.to(user.room).emit('message', { user: user.name, text: message });
+    io.to(user.room).emit('message', { user: user.name, text: message })
 
-    callback();
-  });
+    callback()
+  })
 
   socket.on('disconnect', () => {
-    console.log('User has disconnected!!!')
+    const user = removeUser(socket.id)
+
+    if (user) {
+      io.to(user.room).emit('message', {
+        user: 'Admin',
+        text: `${user.name} has left.`,
+      })
+      io.to(user.room).emit('roomData', {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      })
+    }
   })
 })
-
-app.use(router)
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
